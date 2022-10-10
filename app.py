4fc29b70f37d8625955a597ad65025de2a10f873
Mukeshkannan D <106548923+mukeshinchan@users.noreply.github.com>
2022-10-10 17:01:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,11 @@ if uploaded_file is not None:
             break
         elif polling_response.json()['status'] == 'error':
             raise Exception("Transcription failed. Make sure a valid API key has been used.")
-str=''
+    with open('readme.txt', 'w') as file:
         for speaker in polling_response.json()['utterances']:
             note=f'Speaker {speaker.get("speaker")} : {speaker.get("text")}'
             st.write(note)
-            str=str+note
-st.write(str)
+            file.write(note)
+            file.write('\n')
+    with open('readme.txt','r')as file:
+        st.download_button('Download',file, 'readme')
