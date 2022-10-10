cd0d923b72c161d9bd6778c796f10f1b366d89c5
Mukeshkannan D <106548923+mukeshinchan@users.noreply.github.com>
2022-10-10 13:11:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,9 +23,6 @@ if uploaded_file is not None:
         polling_response = requests.get(TRANSCRIPTION_ENDPOINT + "/" + _id, headers=headers)
         n=n+1
         st.write(n)
-        for speaker in polling_response.json()['utterances']:
-            note=f'Speaker {speaker.get("speaker")} : {speaker.get("text")}'
-        st.write(note)
         if polling_response.json()['status'] == 'completed':
             st.header('Audio To Text Converter')
             break
